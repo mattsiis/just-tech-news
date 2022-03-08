597b56de4aa298ab9533a53e30d2238484356953
mattsiis <35969076+mattsiis@users.noreply.github.com>
2022-03-07 23:26:33 -0500

    hash password

--- models/User.js
+++ models/User.js
@@ -1,6 +1,8 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
+const bcrypt = require('bcrypt');
+
 // create our User model
 class User extends Model {}
 
@@ -46,6 +48,18 @@ User.init(
         }
     },
     {
+        hooks: {
+            // set up beforeCreate lifecycle "hook" functionality
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData
+            },
+            // set up beforeupdate lifecycle "gook" functionality
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
         // table configuration options go here
 
         // pass in our imported sequelize connection
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -50,7 +50,9 @@ router.post('/', (req, res) => {
 
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
+    // pass in req.body instead to only update what's passed through
     User.update(req.body, {
+        individualHooks: true,
         where: {
             id: req.params.id
         }
